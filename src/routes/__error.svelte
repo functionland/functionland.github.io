4d71de80ec6302c2cf40942824fe47e94b02e88f
Mohammad Hossein Shakouri <mhshakouri@gmail.com>
2022-02-14 16:04:09 +0000

    error layouts / components

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -2,7 +2,7 @@
 	export const prerender = true;
 	/** @type {import('@sveltejs/kit').ErrorLoad} */
 	export function load({ error, status }) {
-		console.log(status)
+		console.log(status);
 		return {
 			props: {
 				message: error.message,
@@ -11,9 +11,11 @@
 		};
 	}
 </script>
+
 <script>
 	import ErrorScreenHandler from '$lib/components/error/screenHandler.svelte';
 	export let message;
 	export let status;
 </script>
-<ErrorScreenHandler message={message} status={status} />
+
+<ErrorScreenHandler {message} {status} />
