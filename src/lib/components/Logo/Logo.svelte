b181efe314dd4173df6421016d95fefc95939dbb
Mohammad Hossein Shakouri <mhshakouri@gmail.com>
2022-02-14 15:59:50 +0000

    improved logo component

--- src/lib/components/Logo/Logo.svelte
+++ src/lib/components/Logo/Logo.svelte
@@ -1,24 +1,24 @@
 <script context="module">
 	export const prerender = true;
-    import GetLogo from './getLogo.svelte';
-</script>
-<script>
-    export let type;
-    import logoWhite from '$lib/img/layout/logo-w.svelte';
-    import logoBlack from '$lib/img/layout/logo-b.svelte';
-    import logoWhitePortrait from '$lib/img/layout/logo-p-w.svelte';
-    import logoBlackPortrait from '$lib/img/layout/logo-p-b.svelte';
-    import logoWhiteMark from '$lib/img/layout/logo-m-w.svelte';
-    import logoBlackMark from '$lib/img/layout/logo-m-b.svelte';
 </script>
 
-{#if type==="portrait"}
-    <svelte:component this={logoWhitePortrait} width="166px" height="109px" />
-    <svelte:component this={logoBlackPortrait} width="166px" height="109px" />
-{:else if type==="mark"}
-    <svelte:component this={logoWhiteMark} width="157px" height="35px" />
-    <svelte:component this={logoBlackMark} width="157px" height="35px" />
+<script>
+	export let type;
+	import LogoWhite from '$lib/img/layout/logo-w.svelte';
+	import LogoBlack from '$lib/img/layout/logo-b.svelte';
+	import LogoWhitePortrait from '$lib/img/layout/logo-p-w.svelte';
+	import LogoBlackPortrait from '$lib/img/layout/logo-p-b.svelte';
+	import LogoWhiteMark from '$lib/img/layout/logo-m-w.svelte';
+	import LogoBlackMark from '$lib/img/layout/logo-m-b.svelte';
+</script>
+
+{#if type === 'portrait'}
+	<LogoWhitePortrait width="166px" height="109px" />
+	<LogoBlackPortrait width="166px" height="109px" />
+{:else if type === 'mark'}
+	<LogoWhiteMark width="157px" height="35px" />
+	<LogoBlackMark width="157px" height="35px" />
 {:else}
-    <svelte:component this={logoWhite} width="157px" height="35px" />
-    <svelte:component this={logoBlack} width="157px" height="35px" />
+	<LogoWhite width="157px" height="35px" />
+	<LogoBlack width="157px" height="35px" />
 {/if}
--- src/lib/components/Logo/getLogo.svelte
+++
@@ -1,5 +0,0 @@
-<script context="module">
-	export const prerender = true;
-</script>
-<script>
-</script>
