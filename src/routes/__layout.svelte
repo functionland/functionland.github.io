6f5d854ee56bdd9a40de463d9679fe34cef61248
Mohammad Hossein Shakouri <mhshakouri@gmail.com>
2022-02-14 16:05:51 +0000

    style path change on layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,12 @@
 <script>
 	import Header from '$lib/header/Header.svelte';
 	import Footer from '$lib/footer/Footer.svelte';
-	import '../app.css';
+	import '../styles/app.css';
 	import '../main.js';
 </script>
+
 <Header />
 <main>
 	<slot />
 </main>
 <Footer />
-<style>
-	main {
-		width: 100%;
-    	max-width: 100vw;
-		grid-row: 2/3;
-	}
-</style>
