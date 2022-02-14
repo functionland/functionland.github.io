60c9b244ac108ce00aef2b31bac49107312494a1
Mohammad Hossein Shakouri <mhshakouri@gmail.com>
2022-02-14 16:38:24 +0000

    created back to top component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from '$lib/header/Header.svelte';
 	import Footer from '$lib/footer/Footer.svelte';
+	import BackToTop from '$lib/components/back-to-top/backToTop.svelte';
 	import '../styles/app.css';
 </script>
 
@@ -9,3 +10,4 @@
 	<slot />
 </main>
 <Footer />
+<BackToTop />
