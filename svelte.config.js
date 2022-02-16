9b1553f4e0bf41b43fedc6117962e48f8d856a51
Mohammad Hossein Shakouri <mhshakouri@gmail.com>
2022-02-16 01:38:52 +0000

    created some commented config for postcss to use later

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
-
+// import adapter from '@sveltejs/adapter-node';
+// import preprocess from 'svelte-preprocess';
+// import importAssets from 'svelte-preprocess-import-assets';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -8,3 +10,32 @@ const config = {
 };
 
 export default config;
+
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			// default options are shown
+// 			out: 'build',
+// 			precompress: false,
+// 			env: {
+// 				path: 'SOCKET_PATH',
+// 				host: 'HOST',
+// 				port: 'PORT',
+// 				origin: 'ORIGIN',
+// 				headers: {
+// 					protocol: 'PROTOCOL_HEADER',
+// 					host: 'HOST_HEADER'
+// 				}
+// 			}
+// 		}),
+// 		preprocess: preprocess({
+// 			importAssets()
+// 		}),
+// 		preprocess: [importAssets()],
+// 		vite: {
+// 			define: {
+// 				'process.env': process.env
+// 			}
+// 		}
+// 	}
+// };
