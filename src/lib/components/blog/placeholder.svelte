73b21203f43f6bd157b51604e3fea669b7bdac12
Mohammad Hossein Shakouri <mhshakouri@gmail.com>
2022-02-18 16:52:10 +0000

    Added : blog items
    * css only for mobile

---
+++ src/lib/components/blog/placeholder.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+	export const prerender = false;
+	export const hydrate = false;
+</script>
+<div></div>
+
+<style>
+    div {
+        width: 100%;
+        padding-top: 100%;
+        background-color: #C4C4C4;
+    }
+</style>
--- src/lib/homepage/news.svelte
+++ src/lib/homepage/news.svelte
@@ -6,5 +6,37 @@
 <script>
 	import Blog from '$lib/components/blog/index.svelte';
 </script>
+<section>
+	<h4>Latest News</h4>
+	<p>Access your favourite apps without the need to pay!</p>
+	<div>
+		<Blog />
+	</div>
+</section>
 
-<Blog />
+
+<style>
+	section {
+		min-height: var(--section-min-height);
+		padding: 40px 0 80px;
+	}
+	h4 {
+		font-size: var(--section-title-font-size);
+		line-height: var(--section-title-line-height);
+		text-align: center;
+		padding-bottom: 2rem;
+	}
+	p {
+		max-width: calc(100% - 120px);
+		margin: 0 auto;
+		text-align: center;
+		padding-bottom: 20px;
+	}
+	div {
+		display: grid;
+		grid-auto-flow: row;
+		grid-row-gap: 16px;
+		max-width: calc(100% - 32px);
+		margin: 0 auto;
+	}
+</style>
