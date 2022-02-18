87a09fac035bb0741ecaec1a78897e8389297780
Mohammad Hossein Shakouri <mhshakouri@gmail.com>
2022-02-18 11:34:51 +0000

    trying to make the earn-crypto loop, better to get two seperate videos for loop

--- src/lib/components/headline/videos.svelte
+++ src/lib/components/headline/videos.svelte
@@ -3,10 +3,24 @@
 </script>
 <script>
     import  { prefersColorScheme } from 'svelte-window-stores/appearance';
+	import { onMount } from 'svelte';
     export let data;
 	let videos = data.videos ? data.videos.length > 0 ? data.videos : false : false;
     let browserSupportText = "Your browser does not support the video element.";
 	$: colorScheme = $prefersColorScheme === 0 ? "light" : "dark";
+	// let ended
+	// $: loopClass = ended == true ? "visible" : "hidden";
+	// $: endedClass = ended == true ? "hidden" : "visible";
+	// let time
+	// const loopTime = (event)=> {
+	// 	console.log(time)
+	// 	if (time < 5 || time > 5.9)
+	// 	 {
+	// 		 time = 5.5;
+	// 	 }
+
+	// }
+	
 </script>
 {#if videos != false}
 	<div class="{data.ref}">
@@ -17,7 +31,18 @@
 					<!-- <Video video={video} ref={item.ref}/> -->
 				{/if}
 			{:else}
-				<video src={video.src} autoplay loop playsinline muted class="{data.ref}">{browserSupportText}</video>
+				{#if data.ref === "earn-crypto"} 
+					<video autoplay playsinline muted class="{data.ref}">
+						<source src={video.src} type={video.type} />
+						{browserSupportText}
+					</video>
+					<!-- <video autoplay loop playsinline muted bind:currentTime={time} on:timeupdate={loopTime} class="{data.ref} main {loopClass}">
+						<source src={video.src} type={video.type} />
+						{browserSupportText}
+					</video> -->
+				{:else}
+					<video src={video.src} autoplay loop playsinline muted class="{data.ref}">{browserSupportText}</video>
+				{/if}
 				<!-- <Video video={video} ref={item.ref}/> -->
 			{/if}
 		{/each}
@@ -65,5 +90,11 @@
 		top: calc( 100% - (100% - 90px));
 		left: 50%;
 		transform: translateX(-50%);
+	}
+	video.visible {
+		z-index: 1;
+	}
+	video.hidden {
+		z-index: -1;
 	}
 </style>
