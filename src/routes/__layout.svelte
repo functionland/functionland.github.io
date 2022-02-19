cc2d740e565d2a02213c15c25e5cc045a87040b7
Mohammad Hossein Shakouri <mhshakouri@gmail.com>
2022-02-19 05:30:16 +0000

    Changed: the way animations are imported

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,14 @@
 <script context="module">
 	export const prerender = true;
+	/** @type {import('@sveltejs/kit').Load} */
+	export const load = async ({ url }) => ({ props: { url } });
 </script>
-
 <script>
 	import Header from '$lib/header/index.svelte';
 	import Footer from '$lib/footer/index.svelte';
 	// import BackToTop from '$lib/components/back-to-top/index.svelte';
 	import '../styles/app.css';
+	import '../styles/animations.css';
 </script>
 
 <Header />
